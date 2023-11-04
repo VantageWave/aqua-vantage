abde4effc832240b8a4e6a5431d85041f7cec031
jczuchra <czuchra.jakub@gmail.com>
2023-11-04 17:43:25 +0100

    fix: fix api

--- src/app/utils/clientUrls.ts
+++ src/app/utils/clientUrls.ts
@@ -1,4 +1,4 @@
 const host = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : process.env.HOSTNAME;
 
-export const getCatalogsUrl = () => `${host}/api/catalogs`;
-export const getCatalogUrl = (id: string) => `${host}/api/catalog/${id}`;
+export const getCatalogsUrl = () => `/api/catalogs`;
+export const getCatalogUrl = (id: string) => `/api/catalog/${id}`;
